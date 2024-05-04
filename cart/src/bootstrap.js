0887ae96061c912889d1d13592d0f7b43d85e2d3
evertonsavio <everluca@outlook.com>
2024-05-04 19:39:18 -0300

    :rocket: execution context: isolated dev vs prod

--- cart/src/bootstrap.js
+++ cart/src/bootstrap.js
@@ -1,4 +1,32 @@
 import faker from 'faker';
 
-const cartText = `<div>You have ${faker.random.number()} items in your cart</div>`;
-document.querySelector('#dev-cart').innerHTML = cartText;
+const mount = (el) => {
+
+    const cartText = `<div>You have ${faker.random.number()} items in your cart</div>`;
+
+    el.innerHTML = cartText;
+    // on React would be: ReactDOM.render(<App />, el);
+};
+
+// Context/Situation #1
+// We are running this file in development in isolation
+// We are using our local index.html file
+// Which DEFINITELY has an element with an id of 'dev-products'
+// We want to immediately render our app into that element
+if (process.env.NODE_ENV === 'development') {
+    const el = document.querySelector("#dev-cart");
+
+    // Assuming our container doesnt have an #dev-products element
+    if (el) {
+        // We are probably running in isolation
+        mount(el);
+    }
+};
+
+// Context/Situaltion #2
+// We are running this file in development or production
+// through the CONTAINER app
+// NO GUARANTEE that an element with an id of 'dev-products' exists
+// We do not want try to immediately render the app
+
+export { mount };
--- products/src/bootstrap.js
+++ products/src/bootstrap.js
@@ -1,9 +1,36 @@
 import faker from 'faker';
 
-let products = '';
+const mount = (el) => {
 
-for (let i = 0; i < 10; i++) {
-    products += `<div>${faker.commerce.productName()}</div>`;
+    let products = '';
+
+    for (let i = 0; i < 10; i++) {
+        products += `<div>${faker.commerce.productName()}</div>`;
+    };
+
+    el.innerHTML = products;
+    // on React would be: ReactDOM.render(<App />, el);
 };
 
-document.querySelector('#dev-products').innerHTML = products;
+// Context/Situation #1
+// We are running this file in development in isolation
+// We are using our local index.html file
+// Which DEFINITELY has an element with an id of 'dev-products'
+// We want to immediately render our app into that element
+if (process.env.NODE_ENV === 'development') {
+    const el = document.querySelector("#dev-products");
+
+    // Assuming our container doesnt have an #dev-products element
+    if (el) {
+        // We are probably running in isolation
+        mount(el);
+    }
+};
+
+// Context/Situaltion #2
+// We are running this file in development or production
+// through the CONTAINER app
+// NO GUARANTEE that an element with an id of 'dev-products' exists
+// We do not want try to immediately render the app
+
+export { mount };
