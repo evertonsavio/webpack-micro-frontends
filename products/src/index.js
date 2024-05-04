74b1d51f65825f7e0b818f9f98a2e418a92e89cf
evertonsavio <everluca@outlook.com>
2024-05-04 18:40:50 -0300

    :package: shared fake module

--- products/src/index.js
+++ products/src/index.js
@@ -1,9 +1,1 @@
-import faker from 'faker';
-
-let products = '';
-
-for (let i = 0; i < 10; i++) {
-    products += `<div>${faker.commerce.productName()}</div>`;
-};
-
-document.querySelector('#dev-products').innerHTML = products;
+import('./bootstrap')
