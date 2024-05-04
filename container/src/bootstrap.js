712f7a020ad1b2b5e6b8d42e4a8c8e0be0a88ccd
evertonsavio <everluca@outlook.com>
2024-05-04 19:38:29 -0300

    :rocket: execution context: isolated dev vs prod

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,4 +1,9 @@
-import 'products/ProductsIndex';
+import { mount as productMout } from 'products/ProductsIndex';
+import { mount as cartMount } from 'cart/CartShow';
 import 'cart/CartShow';
 
 console.log("Container!")
+
+// Never assign an id that is equal to the name of a global variable on webpack.config.js at ModuleFederationPlugin
+productMout(document.querySelector("#my-products"));
+cartMount(document.querySelector("#my-cart"));
